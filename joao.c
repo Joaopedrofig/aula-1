3bb46ad43792cb4f0433e01865dcc0c3c53228e2
Joaopedrofig <joaopedrofigueiredo741@gmail.com>
2024-08-13 21:04:47 -0300

    Add files via upload

---
+++ joao.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int numero = 20, Numero = 5;
+    int soma = numero+Numero;
+    int subtracao = numero-Numero;
+    float divisao = numero/Numero;
+    int multiplicacao = numero*Numero;
+    printf("Seu numero e: %.f",divisao);
+
+
+}
